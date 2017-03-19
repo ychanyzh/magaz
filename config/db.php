b4de2bb9dae229ce6adcd6819057aa2fc134fdc8
Shakhlin <vodolley.1992@mail.ru>
2017-03-19 16:18:31 +1000

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2.loc',
     'username' => 'root',
-    'password' => 'pingvin9812889',
+    'password' => '',
     'charset' => 'utf8',
 ];
